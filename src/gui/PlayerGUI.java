bc504a52f8015d68a6f2f1a0931ce403e9a9139d
kirluc11 <kirluc11@htlkaindorf.at>
2015-05-22 09:07:34 +0200

    implemented filter for ip

--- src/gui/PlayerGUI.java
+++ src/gui/PlayerGUI.java
@@ -19,8 +19,7 @@ import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
  *
  * @author Churchy
  */
-public class PlayerGUI extends javax.swing.JFrame
-{
+public class PlayerGUI extends javax.swing.JFrame {
 
     private JPanel aktPanel;
     private boolean connected = false;
@@ -29,8 +28,7 @@ public class PlayerGUI extends javax.swing.JFrame
     /**
      * Creates new form PlayerGUI
      */
-    public PlayerGUI()
-    {
+    public PlayerGUI() {
         initComponents();
         this.setSize(600, 600);
         this.setLocationRelativeTo(null);
@@ -41,8 +39,7 @@ public class PlayerGUI extends javax.swing.JFrame
         showGameChooser();
     }
 
-    public void showGameChooser()
-    {
+    public void showGameChooser() {
         GameChooserPanel gcp = new GameChooserPanel(pnGame, gClient);
         aktPanel = gcp;
         pnGame.removeAll();
@@ -57,8 +54,7 @@ public class PlayerGUI extends javax.swing.JFrame
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents()
-    {
+    private void initComponents() {
 
         pnTop = new javax.swing.JPanel();
         pnServer = new javax.swing.JPanel();
@@ -93,10 +89,8 @@ public class PlayerGUI extends javax.swing.JFrame
         pnServer.add(pnPort);
 
         btConnect.setText("Connect");
-        btConnect.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
+        btConnect.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 onDisConnect(evt);
             }
         });
@@ -105,13 +99,6 @@ public class PlayerGUI extends javax.swing.JFrame
         pnTop.add(pnServer, java.awt.BorderLayout.CENTER);
 
         btHome.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/pictures/home.png"))); // NOI18N
-        btHome.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                onHome(evt);
-            }
-        });
         pnTop.add(btHome, java.awt.BorderLayout.EAST);
 
         getContentPane().add(pnTop, java.awt.BorderLayout.NORTH);
@@ -125,16 +112,12 @@ public class PlayerGUI extends javax.swing.JFrame
 
     private void onHome(java.awt.event.ActionEvent evt)//GEN-FIRST:event_onHome
     {//GEN-HEADEREND:event_onHome
-        if(gClient.isConnected())
-        {
-            try
-            {
+        if (gClient.isConnected()) {
+            try {
                 gClient.sendObject("##GO##HOME##");
-            } catch (IOException ex)
-            {
+            } catch (IOException ex) {
                 Logger.getLogger(PlayerGUI.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ClassNotFoundException ex)
-            {
+            } catch (ClassNotFoundException ex) {
                 Logger.getLogger(PlayerGUI.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
@@ -143,77 +126,85 @@ public class PlayerGUI extends javax.swing.JFrame
 
     private void onDisConnect(java.awt.event.ActionEvent evt)//GEN-FIRST:event_onDisConnect
     {//GEN-HEADEREND:event_onDisConnect
-        if (!connected)
-        {
-            String nickname = JOptionPane.showInputDialog("Please enter nickname");
-            String inetAddress = tfIP.getText();
-            String portString = tfPort.getText();
-            if (!inetAddress.isEmpty() && !portString.isEmpty())
-            {
-                try
-                {
-                    gClient.setPORTNR(Integer.parseInt(portString));
-                    gClient.setAddress(inetAddress);
-                    gClient.startClient();
-                    gClient.setNickname(nickname);
-                    connected = true;
-                    btConnect.setText("Disconnect");
-                } catch (IOException ex)
-                {
+        try {
+            if (!connected) {
+                String inetAddress = tfIP.getText();
+                String[] ipParts = inetAddress.split("\\.");
+                if (ipParts.length != 4) {
+                    throw new Exception("Invalid IP address!");
+                }
+                for (String ipPart : ipParts) {
+                    for (int i = 0; i < ipPart.length(); i++) {
+                        if (ipPart.toUpperCase().charAt(i) < '0' || ipPart.toUpperCase().charAt(i) > '9') {
+                            throw new Exception("Invalid IP address!\n"
+                                    + "Numbers only!");
+                        }
+                    }
+                }
+
+                for (String ipPart : ipParts) {
+                    int num = Integer.parseInt(ipPart);
+                    if (num < 0 || num > 255) {
+                        throw new Exception("Invalid IP address!\n"
+                                + "Only number from 0 to 255!");
+                    }
+                }
+
+                String nickname = JOptionPane.showInputDialog("Please enter nickname");
+                String portString = tfPort.getText();
+                if (!inetAddress.isEmpty() && !portString.isEmpty()) {
+                    try {
+                        gClient.setPORTNR(Integer.parseInt(portString));
+                        gClient.setAddress(inetAddress);
+                        gClient.startClient();
+                        gClient.setNickname(nickname);
+                        connected = true;
+                        btConnect.setText("Disconnect");
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(this, "An error occurred", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(this, "Please enter valid portnumber", "Invalid port", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            } else {
+                try {
+                    gClient.stopClient();
+                    connected = false;
+                    btConnect.setText("Connect");
+                } catch (IOException ex) {
                     JOptionPane.showMessageDialog(this, "An error occurred", "ERROR", JOptionPane.ERROR_MESSAGE);
-                } catch (NumberFormatException ex)
-                {
-                    JOptionPane.showMessageDialog(this, "Please enter valid portnumber", "Invalid port", JOptionPane.ERROR_MESSAGE);
                 }
             }
-        } else
-        {
-            try
-            {
-                gClient.stopClient();
-                connected = false;
-                btConnect.setText("Connect");
-            } catch (IOException ex)
-            {
-                JOptionPane.showMessageDialog(this, "An error occurred", "ERROR", JOptionPane.ERROR_MESSAGE);
-            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, ex.getMessage(), "IP-ERROR", JOptionPane.ERROR_MESSAGE);
         }
     }//GEN-LAST:event_onDisConnect
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
 
-        try
-        {
+        try {
             /* Set the Nimbus look and feel */
             //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
             /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
              * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
              */
-            try
-            {
-                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
-                {
-                    if ("Nimbus".equals(info.getName()))
-                    {
+            try {
+                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
                         javax.swing.UIManager.setLookAndFeel(info.getClassName());
                         break;
                     }
                 }
-            } catch (ClassNotFoundException ex)
-            {
+            } catch (ClassNotFoundException ex) {
                 java.util.logging.Logger.getLogger(PlayerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            } catch (InstantiationException ex)
-            {
+            } catch (InstantiationException ex) {
                 java.util.logging.Logger.getLogger(PlayerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            } catch (IllegalAccessException ex)
-            {
+            } catch (IllegalAccessException ex) {
                 java.util.logging.Logger.getLogger(PlayerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            } catch (javax.swing.UnsupportedLookAndFeelException ex)
-            {
+            } catch (javax.swing.UnsupportedLookAndFeelException ex) {
                 java.util.logging.Logger.getLogger(PlayerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
             }
             //</editor-fold>
@@ -223,16 +214,13 @@ public class PlayerGUI extends javax.swing.JFrame
             UIManager.put("RootPane.setupButtonVisible", false);
 
             /* Create and display the form */
-            java.awt.EventQueue.invokeLater(new Runnable()
-            {
-                public void run()
-                {
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
                     new PlayerGUI();
                     new ServerGUI().setVisible(true);
                 }
             });
-        } catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Logger.getLogger(PlayerGUI.class.getName()).log(Level.SEVERE, null, ex);
         }
         //</editor-fold>
