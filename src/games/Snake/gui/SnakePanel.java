fb05e14d03ced33ab25f3a0973ee245f82eb40f5
smimac11 <smimac11@htlkaindorf.at>
2015-06-19 08:52:24 +0200

    Revert "Snake Restart"
    This reverts commit 5c7a8f0c80421095ffc4c0534010f38b6f7d0ef5.

--- src/games/Snake/gui/SnakePanel.java
+++ src/games/Snake/gui/SnakePanel.java
@@ -60,28 +60,11 @@ public class SnakePanel extends javax.swing.JPanel implements Runnable, Directio
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents()
-    {
-
-        pmRestart = new javax.swing.JPopupMenu();
-        miRestart = new javax.swing.JMenuItem();
-
-        miRestart.setText("Restart");
-        miRestart.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                onMiRestart(evt);
-            }
-        });
-        pmRestart.add(miRestart);
+    private void initComponents() {
 
         setBackground(new java.awt.Color(0, 0, 0));
-        setComponentPopupMenu(pmRestart);
-        addKeyListener(new java.awt.event.KeyAdapter()
-        {
-            public void keyTyped(java.awt.event.KeyEvent evt)
-            {
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
                 onKeyTyped(evt);
             }
         });
@@ -116,15 +99,6 @@ public class SnakePanel extends javax.swing.JPanel implements Runnable, Directio
         }
         snake.changeDirection(dir);
     }//GEN-LAST:event_onKeyTyped
-
-    private void onMiRestart(java.awt.event.ActionEvent evt)//GEN-FIRST:event_onMiRestart
-    {//GEN-HEADEREND:event_onMiRestart
-        if(!thread.interrupted())
-        {
-            endGame();
-        }
-        startGame();
-    }//GEN-LAST:event_onMiRestart
 
     public void restart()
     {
@@ -167,13 +141,11 @@ public class SnakePanel extends javax.swing.JPanel implements Runnable, Directio
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenuItem miRestart;
-    private javax.swing.JPopupMenu pmRestart;
     // End of variables declaration//GEN-END:variables
     @Override
     public void run() {
         try {
-            while (!Thread.interrupted()&&snake.move()) {
+            while (snake.move()&&!Thread.interrupted()) {
                 try {
                     Thread.sleep(150);
                     repaint();
@@ -181,10 +153,7 @@ public class SnakePanel extends javax.swing.JPanel implements Runnable, Directio
                 }
             }
         } catch (IOException ex) {
-            
-        }catch(NullPointerException ex)
-        {
-            //System.out.println(ex.toString());
+            Logger.getLogger(SnakePanel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
